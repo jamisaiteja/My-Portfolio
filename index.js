7ed748371e8ad36fc5eb20f033169685de169b72
jamisaiteja <jamisaiteja666@gmail.com>
2022-10-29 16:55:19 +0530

    commit 29th

--- index.js
+++ index.js
@@ -1,25 +1,46 @@
-// // const a = window.location.hash;
-// // console.log(a);
-
 const sections = document.querySelectorAll("section");
 const nava = document.querySelectorAll("nav .collapse div a");
 window.addEventListener("scroll", () => {
     let current = "";
     sections.forEach((section) => {
         const sectionTop = section.offsetTop;
-        console.log(sectionTop)
         const sectionHeight = section.clientHeight;
-        console.log(sectionHeight)
-        if (scrollY >= sectionTop - sectionHeight / 3) {
+        if (scrollY >= (sectionTop - sectionHeight / 3)) {
+            console.log(scrollY)
             current = section.getAttribute("id");
         }
     });
-    console.log(current)
 
     nava.forEach((a) => {
         a.classList.remove("active");
         if (a.classList.contains(current)) {
             a.classList.add("active");
         }
+    });
+});
+
+
+
+// ------------------
+// Form
+// ------------------
+
+let form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    document.querySelector("#sub").value = "Sending..";
+    let data = new FormData(form);
+    fetch(
+        "https://script.google.com/macros/s/AKfycbw2zqXksryaqaWmye4qMZpeFiQUGKRdRdxgvfbM8tJcyyMRS8CLlucJIjWggx8E_9-D/exec",
+        {
+        method: "POST",
+        body: data
+        }
+    )
+    .then((res) => res.text())
+    .then((data) => {
+        console.log(data);
+        document.querySelector("#msg").innerHTML = data;
+        document.querySelector("#sub").value = "Submit";
     });
 });
