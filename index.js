f62287917cedee595781dbcead1e0f525a258734
jamisaiteja <jamisaiteja666@gmail.com>
2022-11-09 14:35:40 +0530

    commit-nov11-1

--- index.js
+++ index.js
@@ -34,38 +34,25 @@ function sendEmail(){
     var sub = name +"'s message from portfolio contact form!";
 
     Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "jamisaiteja666@gmail.com",
-        Password : "46207ED15A8729E402E72B0AC7093DEB1AD1",
+        // Host : "smtp.elasticemail.com",
+        // Username : "jamisaiteja666@gmail.com",
+        // Password : "46207ED15A8729E402E72B0AC7093DEB1AD1",
+        SecureToken:"1482fe7a-9641-485d-9d31-66beaf870ef4",
         To : 'saitejajami6@gmail.com',
         From : "jamisaiteja666@gmail.com",
         Subject : sub,
         Body : body
     }).then(
         message => alert("Message Sent Successfully to Jami Saiteja")
-    );
+    ).catch(err => {
+        alert(err)
+    })
 }
 
 
-// let url="https://script.google.com/macros/s/AKfycbxHRrGg17AqZF9iz8-SgUAJHSLrsVQg2oGdtMkuO-DXL1JzZ4LHs6sqp85nk6G9Loly/exec";
-// let form = document.querySelector("#form");
-//         let submit = document.querySelector("#sub");
-//         let message = document.querySelector("#span");
-//         form.addEventListener('submit', (e) => {
-//             e.preventDefault();
-//             submit.value = "Sending.."
-//             fetch(url, {
-//                     method: "POST",
-//                     body: new FormData(form)
-//                 })
-//                 .then(res => res.text())
-//                 .then(data => {
-//                     message.innerHTML = data;
-//                     submit.value = "Send"
+// ------------------
+// footer
+// ------------------
 
-//                 })
-//                 .catch(err => {
-//                     message.innerHTML = err;
-//                     submit.value = "Send"
-//                 })
-//         })
+let year = document.getElementById("year");
+year.textContent=new Date().getFullYear();
